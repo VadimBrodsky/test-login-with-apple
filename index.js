1f3896a2583e3ac1a17a78a7cdb1d4f89231a444
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-17 16:13:28 -0400

    Add base server

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+let Hapi = require('@hapi/hapi');
+
+let init = async () => {
+  let server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log('Server is running on %s', server.info.url);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit();
+});
+
+init();
