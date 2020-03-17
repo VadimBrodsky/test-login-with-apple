d2b588187bf8df9111ce0128f3d06df4c12d27a2
Vadim Brodsky <vadim.brodsky@gmail.com>
2020-03-17 17:36:25 -0400

    Add views and layouts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
+let Path = require('path');
 let Hapi = require('@hapi/hapi');
+let Hoek = require('@hapi/hoek');
 
 let init = async () => {
   let server = Hapi.server({
@@ -7,11 +9,27 @@ let init = async () => {
     host: 'localhost',
   });
 
+  await server.register(require('@hapi/vision'));
+
+  server.views({
+    engines: {
+      html: {
+        module: require('handlebars'),
+        compileMode: 'sync',
+        isCached: process.env.NODE_ENV === 'production',
+      },
+    },
+    layout: 'default',
+    layoutPath: 'templates/layouts',
+    path: 'templates',
+    relativeTo: __dirname,
+  });
+
   server.route({
     method: 'GET',
     path: '/',
     handler: (request, h) => {
-      return 'Hello World!';
+      return h.view('index', { title: 'Homepage' });
     },
   });
 
